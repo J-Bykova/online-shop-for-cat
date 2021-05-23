e0afedeb5871d0926e9bab73b57982eb5a537e0b
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-05-23 16:06:40 -0700

    Try to fix the slider on desktop

--- source/js/slider.js
+++ source/js/slider.js
@@ -26,29 +26,29 @@ function showBefore() {
 // showBefore();
 
 
-sliderHandle.addEventListener("mousedown", onSideBarCatch);
-window.addEventListener("mouseup", onSideBarRelease);
-
-const sliderRange = calculateSliderRange();
-let initialPosition;
-function onSideBarCatch(e) {
-  initialPosition = e.clientX;
-  window.addEventListener("mousemove", onMouseMove);
-}
-
-function onSideBarRelease() {
-  window.removeEventListener("mousemove", onMouseMove);
-}
-
-function onMouseMove(e) {
-  const offset = Math.round(e.clientX - initialPosition);
-  const currentLeft = parseInt(window.getComputedStyle(sliderHandle).left)
-
-  sliderHandle.style.left = currentLeft + offset + "px";
-  // console.log(currentLeft)
-}
-
-
-function calculateSliderRange() {
-  return parseInt(window.getComputedStyle(sliderBar).width);
-}
+// sliderHandle.addEventListener("mousedown", onSideBarCatch);
+// window.addEventListener("mouseup", onSideBarRelease);
+//
+// const sliderRange = calculateSliderRange();
+// let initialPosition;
+// function onSideBarCatch(e) {
+//   initialPosition = e.clientX;
+//   window.addEventListener("mousemove", onMouseMove);
+// }
+//
+// function onSideBarRelease() {
+//   window.removeEventListener("mousemove", onMouseMove);
+// }
+//
+// function onMouseMove(e) {
+//   const offset = Math.round(e.clientX - initialPosition);
+//   const currentLeft = parseInt(window.getComputedStyle(sliderHandle).left)
+//
+//   sliderHandle.style.left = currentLeft + offset + "px";
+//   // console.log(currentLeft)
+// }
+//
+//
+// function calculateSliderRange() {
+//   return parseInt(window.getComputedStyle(sliderBar).width);
+// }
